d225280ade50d4a5faa1b1f2987419894df656fd
Chris Fried <fried.chris@gmail.com>
2016-02-17 08:48:14 -0600

    Better status.

--- public/js/time-counter.js
+++ public/js/time-counter.js
@@ -3,6 +3,10 @@ var httpGetAsync = function(theUrl, callback, variables){
   xmlHttp.onreadystatechange = function() { 
     if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
       callback(xmlHttp.responseText, variables);
+    else if (xmlHttp.readyState == 4) {
+      stopSpinner();
+      updateStatus('Something went wrong...', true)
+    }
   }
   xmlHttp.open("GET", theUrl, true); // true for asynchronous 
   xmlHttp.send(null);
@@ -112,7 +116,7 @@ var daysPlayedCallback = function(days, max) {
 
 var characterCallback = function(results, variables) {
   console.log('character callback started')
-  updateStatus('Fetching character data for ' + pathArray[1] + ' on ' + platform + '. This could take awhile...');
+  updateStatus('Fetching character data for ' + username + ' on ' + platform + '. This could take awhile...');
   var json;
   try {
     json = JSON.parse(results);
@@ -163,7 +167,7 @@ var characterCallback = function(results, variables) {
 
 var accountCallback = function(results) {
   console.log('account callback started')
-  updateStatus('Fetching account data for ' + pathArray[1] + ' on ' + platform + '...');
+  updateStatus('Fetching account data for ' + username + ' on ' + platform + '...');
   var json;
   try {
     json = JSON.parse(results);
@@ -188,13 +192,13 @@ var accountCallback = function(results) {
   }
   else {
     stopSpinner();
-    updateStatus('Failed to fetch account data for ' + pathArray[1] + ' on ' + platform, true);
+    updateStatus('Failed to fetch account data for ' + username + ' on ' + platform, true);
   }
 }
 
 var searchCallback = function(results) {
   console.log('search callback started')
-  updateStatus('Searching for ' + pathArray[1] + ' on ' + platform + '...');
+  updateStatus('Searching for ' + username + ' on ' + platform + '...');
   var json;
   try {
     json = JSON.parse(results);
@@ -211,14 +215,15 @@ var searchCallback = function(results) {
   }
   else {
     stopSpinner();
-    updateStatus(pathArray[1] + ' not found on ' + platform, true)
+    updateStatus(username + ' not found on ' + platform, true)
   }
 }
 
 var path = window.location.pathname;
 path = path.slice(1);
 var pathArray = path.split( '/' );
-var searchPath = '/Platform/Destiny/SearchDestinyPlayer/' + pathArray[0] + '/' + pathArray[1] + '/';
+var username = decodeURI(pathArray[1]).trim();
+var searchPath = '/Platform/Destiny/SearchDestinyPlayer/' + pathArray[0] + '/' + username + '/';
 if (pathArray[0] == 2) {
   document.getElementById('platformSwitch').checked = true;
 }
